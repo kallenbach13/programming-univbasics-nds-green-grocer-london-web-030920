ae62236feb2d317e9cfa5d8a119e697075f78159
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-19 16:46:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,1 +1,46 @@
-com/r/DCcomics/comments/f5105z/comic_excerpt_hal_doesnt_fk_around_in_hal_jordan/
+def find_item_by_name_in_collection(name, collection)
+  counter = 0
+  
+  while counter < collection.length do
+    if collection[counter][:item] == name
+      return collection[counter]
+    end
+    counter += 1
+  end
+end
+
+def consolidate_cart(cart)
+  new_cart = []
+  counter = 0
+  
+  while counter < cart.length do
+    new_c
+    if 
+    end
+    counter += 1
+  end
+end
+
+def apply_coupons(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+end
+
+def apply_clearance(cart)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+end
+
+def checkout(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
+end
