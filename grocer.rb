1500bec3975ff05f5d27a59fe9847aa6e3f84460
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-19 23:58:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,7 @@ end
 def apply_coupons(cart, coupons)
   counter = 0
   while counter < coupons.length
-  cart_item = find_item_by_name_in_collection
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
 end
 
 def apply_clearance(cart)
