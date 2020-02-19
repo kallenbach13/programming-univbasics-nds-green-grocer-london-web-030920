0f2b95d3dd488cf5934fd3f53dc79c2e23fc18cd
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-19 16:08:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,8 +2,8 @@ def find_item_by_name_in_collection(name, collection)
   counter = 0
   
   while counter < collection.length do
-    if collection.
-      
+    if collection[counter][:item] == name
+      return collection[counter]
     end
     counter += 1
   end
