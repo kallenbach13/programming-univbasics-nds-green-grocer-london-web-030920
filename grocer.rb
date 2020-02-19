509db2d75bcbea3f3cad2f85d7145e52d1d930fd
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-19 16:50:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,7 @@ def consolidate_cart(cart)
   counter = 0
   
   while counter < cart.length do
-    new_cart = find_item_by_name_in_collection[cart][counter][:item]
+    new_cart = find_item_by_name_in_collection[cart][counter][:item], new_cart)
     if 
     end
     counter += 1
