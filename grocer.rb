d0fc52d640db16fba0f71938349fbf95a76563c9
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-20 00:21:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
         :item => couponed_item_name,
         :price => coupons[counter][:cost] / coupons[counter][:num],
         :count => coupons[counter][:num],
-        :clearance => coupons[counter][:clearance]
+        :clearance => cart_item
       }
       cart << cart_item_with_coupon
       cart_item[:count] -= coupons[counter][:num]
