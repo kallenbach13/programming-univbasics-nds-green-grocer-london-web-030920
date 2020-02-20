64dc9542df35fa1bfee814e605622664ea918390
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-20 00:57:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,4 +72,7 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
   final_cart = apply_clearance(couponed_cart)
+  
+  total = 0
+  counter = 0
 end
